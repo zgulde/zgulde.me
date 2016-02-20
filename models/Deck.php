2df1439dc19be7718dfcedc9d4de526e28e86580
zgulde <zachgulde@gmail.com>
2016-02-19 23:31:15 -0600

    basic construct function will create a full deck

--- models/Deck.php
+++ models/Deck.php
@@ -1,7 +1,20 @@
 <?php 
 
-require('./Model.php');
+require('Model.php');
 
-class Deck extends Model {
-    
+class Deck {
+    public $cards = []; 
+
+    public function __construct($cards = []){
+        if (empty($cards)){
+            $suits = ['C', 'S', 'H', 'D'];
+            $values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+
+            foreach($suits as $suit){
+                foreach($values as $val){
+                    $this->cards[] = ['suit' => $suit, 'value' => $val];
+                }
+            }
+        }
+    }
 }
