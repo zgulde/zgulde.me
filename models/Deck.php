294fd60c9bbb87805f42b7003635557c30701d50
zgulde <zachgulde@gmail.com>
2016-02-21 15:19:33 -0600

    constructor now stores an array of card objects

--- models/Deck.php
+++ models/Deck.php
@@ -1,20 +1,27 @@
 <?php 
 
-require('Model.php');
+require_once 'Card.php';
 
 class Deck {
     public $cards = []; 
+    public $id    = 0;
 
     public function __construct($cards = []){
+
         if (empty($cards)){
-            $suits = ['C', 'S', 'H', 'D'];
+            $suits  = ['C', 'S', 'H', 'D'];
             $values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
 
             foreach($suits as $suit){
                 foreach($values as $val){
-                    $this->cards[] = ['suit' => $suit, 'value' => $val];
+                    $this->cards[] = new Card(['suit' => $suit, 'value' => $val]);
                 }
             }
+        } else {
+            $this->cards = $cards;
         }
+
+
+
     }
 }
