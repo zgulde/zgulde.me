492d80f74fcf02c07e24eea6b71fd2e1d154e80d
zgulde <zachgulde@gmail.com>
2016-02-21 15:28:29 -0600

    basic bootstrap require file and database connection file

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php 
+
+// this file is intended to be run from the public directory
+
+require_once '../vendor/autoload.php';
+
+require_once '../.env.php';
+require_once '../dbc.php';
+require_once '../models/Card.php';
+require_once '../models/Deck.php';
