5232297a3ffad3e7cbef36aad1e6af904e7b38a8
zgulde <zachgulde@gmail.com>
2016-02-21 15:18:42 -0600

    basic constructor

--- models/Card.php
+++ models/Card.php
@@ -3,4 +3,10 @@
 class Card {
     public $suit  = '';
     public $value = '';
+
+    public function __construct($card){
+        $this->suit   = $card['suit'];
+        $this->value  = $card['value'];
+        $this->string = $this->value . $this->suit;
+    }
 }
