d8c4a46cc962f9a9d1ffb54e3dc2f720246cf3d1
zgulde <zachgulde@gmail.com>
2016-02-22 12:41:03 -0600

    added confirmation to the migration

--- migration.php
+++ migration.php
@@ -3,6 +3,12 @@
 require '.env.php';
 require 'dbc.php';
 
+echo "are you sure you want to run a migration?(yes)\n> ";
+
+if (trim(fgets(STDIN)) !== 'yes'){
+    die();
+}
+
 $createTableQuery = 'CREATE TABLE decks(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     cards TEXT NOT NULL,
@@ -11,3 +17,5 @@ $createTableQuery = 'CREATE TABLE decks(
 
 $dbc->exec('DROP TABLE IF EXISTS decks');
 $dbc->exec($createTableQuery);
+
+echo "migration completed!" . PHP_EOL;
