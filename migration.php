3dcdcf2e95978ed0a0ece6f9cc5b193077cfa03f
zgulde <zachgulde@gmail.com>
2016-02-24 12:16:48 -0600

    added unique id to decks table

--- migration.php
+++ migration.php
@@ -6,12 +6,14 @@ require 'dbc.php';
 echo "are you sure you want to run a migration?(yes)\n> ";
 
 if (trim(fgets(STDIN)) !== 'yes'){
+    echo "exiting... no migrations run" . PHP_EOL;
     die();
 }
 
 $createTableQuery = 'CREATE TABLE decks(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     cards TEXT NOT NULL,
+    unique_id VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 )';
 
